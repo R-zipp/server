cb12ede0df976d90fcbf698757b70822c35bf902
psi5059 <psi5059@naver.com>
2023-10-11 16:29:42 +0900

    first commit

---
+++ src/test/java/com/mtvs/arzip/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtvs.arzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
