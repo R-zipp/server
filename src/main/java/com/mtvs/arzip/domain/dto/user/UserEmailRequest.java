9015005a3154a729b30f452b4041a5058a369261
psi5059 <psi5059@naver.com>
2023-11-08 17:53:30 +0900

    [Feat] : controller post로 분리

---
+++ src/main/java/com/mtvs/arzip/domain/dto/user/UserEmailRequest.java
@@ -0,0 +1,16 @@
+package com.mtvs.arzip.domain.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEmailRequest {
+
+    private String email;
+
+}
